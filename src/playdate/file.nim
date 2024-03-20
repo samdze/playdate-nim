7e9e6a925c66f9df561e94c4d752af04fb44958d
Nycto <James@RoundEights.com>
2024-03-19 19:59:27 -0700

    Centralize file error handling

--- src/playdate/file.nim
+++ src/playdate/file.nim
@@ -15,7 +15,19 @@ type
     SDFileObj {.requiresinit.} = object
         resource: SDFilePtr
         path: string
-    SDFile* = ref SDFileObj 
+    SDFile* = ref SDFileObj
+
+proc requireValidStatus(res: SomeInteger): int {.raises: [IOError], discardable.} =
+    privateAccess(PlaydateFile)
+    if res < 0:
+        raise newException(IOError, $playdate.file.geterr())
+    return res.int
+
+proc requireNotNil[T: pointer](res: T): T {.raises: [IOError].} =
+    privateAccess(PlaydateFile)
+    if res == nil:
+        raise newException(IOError, $playdate.file.geterr())
+    return res
 
 proc `=destroy`(this: var SDFileObj) =
     privateAccess(PlaydateFile)
@@ -29,106 +41,73 @@ proc fileCallback(filename: ConstChar, userdata: pointer) {.cdecl.} =
 proc listFiles*(this: ptr PlaydateFile, path: string, showHidden: bool = false): seq[string] {.raises: [IOError]} =
     privateAccess(PlaydateFile)
     var files = newSeq[string]()
-    var res = this.listfiles(toC(path.cstring), fileCallback, addr(files), if showHidden: 1 else: 0)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    this.listfiles(toC(path.cstring), fileCallback, addr(files), if showHidden: 1 else: 0).requireValidStatus
     return files
 
 proc stat*(this: ptr PlaydateFile, path: string): FileStat {.raises: [IOError]} =
     privateAccess(PlaydateFile)
     var info: FileStat = FileStat()
-    let res = this.stat(path.cstring, addr(info[]))
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    this.stat(path.cstring, addr(info[])).requireValidStatus
     return info
 
 proc exists*(this: ptr PlaydateFile, path: string): bool =
     privateAccess(PlaydateFile)
-    var info: FileStat = FileStat()
-    let res = this.stat(path.cstring, addr(info[]))
-    return res == 0
+    var info: FileStatRaw
+    return this.stat(path.cstring, addr(info)) == 0
 
 proc unlink*(this: ptr PlaydateFile, path: string, recursive: bool) {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = this.unlink(path.cstring, if recursive: 1 else: 0)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    this.unlink(path.cstring, if recursive: 1 else: 0).requireValidStatus
 
 proc mkdir*(this: ptr PlaydateFile, path: string) {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = this.mkdir(path.cstring)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    this.mkdir(path.cstring).requireValidStatus
 
 proc rename*(this: ptr PlaydateFile, fromName: string, to: string) {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = this.rename(fromName.cstring, to.cstring)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    this.rename(fromName.cstring, to.cstring).requireValidStatus
 
 proc close*(this: SDFile) {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = playdate.file.close(this.resource)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    discard playdate.file.close(this.resource).requireValidStatus
 
-proc flush*(this: SDFile): int {.raises: [IOError]} =
+proc flush*(this: SDFile): int {.raises: [IOError], discardable} =
     privateAccess(PlaydateFile)
-    let res = playdate.file.flush(this.resource)
-    if res < 0:
-        raise newException(IOError, $playdate.file.geterr())
-    return res
+    return playdate.file.flush(this.resource).requireValidStatus
 
 proc open*(this: ptr PlaydateFile, path: string, mode: FileOptions): SDFile {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = this.open(path.cstring, mode)
-    if res == nil:
-        raise newException(IOError, $playdate.file.geterr())
-    return SDFile(resource: res, path: path)
+    return SDFile(resource: this.open(path.cstring, mode).requireNotNil, path: path)
 
 proc read*(this: SDFile, length: uint): tuple[bytes: seq[byte], length: int] {.raises: [IOError]} =
     privateAccess(PlaydateFile)
     var buffer = newSeq[byte](length)
-    let res = playdate.file.read(this.resource, addr(buffer[0]), length.cuint)
-    if res < 0:
-        raise newException(IOError, $playdate.file.geterr())
+    let res = playdate.file.read(this.resource, addr(buffer[0]), length.cuint).requireValidStatus
     return (bytes: buffer, length: res.int)
 
 proc read*(this: SDFile): seq[byte] {.raises: [IOError]} =
     let size = playdate.file.stat(this.path).size
     privateAccess(PlaydateFile)
     var buffer = newSeq[byte](size)
-    let res = playdate.file.read(this.resource, addr(buffer[0]), size.cuint)
-    if res < 0:
-        raise newException(IOError, $playdate.file.geterr())
+    playdate.file.read(this.resource, addr(buffer[0]), size.cuint).requireValidStatus
     return buffer
 
 proc readString*(this: SDFile): string {.raises: [IOError].} =
     let size = playdate.file.stat(this.path).size
     privateAccess(PlaydateFile)
     var str = newString(size)
-    let res = playdate.file.read(this.resource, addr(str[0]), size.cuint)
-    if res < 0:
-        raise newException(IOError, $playdate.file.geterr())
+    playdate.file.read(this.resource, addr(str[0]), size.cuint).requireValidStatus
     return str
 
 proc seek*(this: SDFile, pos: int, whence: int) {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = playdate.file.seek(this.resource, pos.cint, whence.cint)
-    if res != 0:
-        raise newException(IOError, $playdate.file.geterr())
+    playdate.file.seek(this.resource, pos.cint, whence.cint).requireValidStatus
 
 proc tell*(this: SDFile): int {.raises: [IOError]} =
     privateAccess(PlaydateFile)
-    let res = playdate.file.tell(this.resource)
-    if res < 0:
-        raise newException(IOError, $playdate.file.geterr())
-    return res
+    return playdate.file.tell(this.resource).requireValidStatus
 
-proc write*(this: SDFile, buffer: seq[byte], length: uint): int {.raises: [IOError]} =
+proc write*(this: SDFile, buffer: seq[byte], length: uint): int {.raises: [IOError], discardable} =
     privateAccess(PlaydateFile)
     if length > 0:
-        let res = playdate.file.write(this.resource, unsafeAddr(buffer[0]), length.cuint)
-        if res < 0:
-            raise newException(IOError, $playdate.file.geterr())
-        return res
+        return playdate.file.write(this.resource, unsafeAddr(buffer[0]), length.cuint).requireValidStatus
