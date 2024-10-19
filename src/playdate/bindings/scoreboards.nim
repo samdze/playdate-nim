bdbfa3dad673e05c497701d6b5bc89e24d6c041f
Nino van Hooff <ninovanhooff@gmail.com>
2024-10-19 22:27:57 +0200

    CHANGE use domain models for score and add addScore

--- src/playdate/bindings/scoreboards.nim
+++ src/playdate/bindings/scoreboards.nim
@@ -27,11 +27,8 @@ type PDBoardsListRaw* {.importc: "PDBoardsList", header: "pd_api.h", bycopy.} = 
     lastUpdated* {.importc: "lastUpdated".}: cuint
     boards* {.importc: "boards".}: ptr PDBoardRaw
 
-  # todo register free functions
-
-
 type PersonalBestCallbackRaw* {.importc: "PersonalBestCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
-# type AddScoreCallbackRaw* = proc (score: ptr PDScoreRaw; errorMessage: cstring) {.cdecl.}
+type AddScoreCallbackRaw* {.importc: "AddScoreCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
 # type BoardsListCallbackRaw* = proc (boards: ptr PDBoardsListRaw; errorMessage: cstring) {.cdecl.}
 # type ScoresCallbackRaw* = proc (scores: ptr PDScoresListRaw; errorMessage: cstring) {.cdecl.}
 
@@ -39,9 +36,9 @@ sdktype:
     type PlaydateScoreboards* {.importc: "const struct playdate_scoreboards", header: "pd_api.h".} = object
         getPersonalBestBinding* {.importc: "getPersonalBest".}: proc (boardId: cstring;
             callback: PersonalBestCallbackRaw): cint {.cdecl, raises: [].}
+        addScoreBinding* {.importc: "addScore".}: proc (boardId: cstring; value: cuint;
+            callback: AddScoreCallbackRaw): cint {.cdecl, raises: [].}
         freeScore* {.importc: "freeScore".}: proc (score: PDScorePtr) {.cdecl, raises: [].}
-        # addScore* {.importc: "addScore".}: proc (boardId: cstring; value: cuint;
-        #     callback: AddScoreCallbackRaw): cint {.cdecl.}
         # getScoreboards* {.importc: "getScoreboards".}: proc (
         #     callback: BoardsListCallbackRaw): cint {.cdecl.}
         # freeBoardsList* {.importc: "freeBoardsList".}: proc (
--- src/playdate/scoreboards.nim
+++ src/playdate/scoreboards.nim
@@ -21,36 +21,47 @@ import bindings/scoreboards {.all.}
 #   privateaccess(PlaydateScoreboards)
 #   playdate.scoreboards.freeScore(this.resource)
 type PDScore* = object of RootObj
-  value*: int32
-  rank*: int32
+  value*: uint32
+  rank*: uint32
   player*: string
 
 type
-  # AddScoreCallback* = proc(score: ptr PDScore, errorMessage: string)
   PersonalBestCallback* = proc(score: PDScore, errorMessage: string)
+  AddScoreCallback* = proc(score: PDScore, errorMessage: string)
   # BoardsListCallback* = proc(boards: ptr PDBoardsList, errorMessage: string)
   # ScoresCallback* = proc(scores: ptr PDScoresList, errorMessage: string)
 
 var privatePersonalBestCallback: PersonalBestCallback
+var privateAddScoreCallback: AddScoreCallback
 
 proc invokePersonalBestCallback(score: PDScorePtr, errorMessage: ConstChar) {.cdecl, raises: [].} =
   if errorMessage != nil:
     privatePersonalBestCallback(PDScore(value: 0, rank: 0, player: ""), $errorMessage)
     return
     
-  let domainScore = PDScore(value: score.value.int32, rank: score.rank.int32, player: $score.player)
+  let domainScore = PDScore(value: score.value.uint32, rank: score.rank.uint32, player: $score.player)
   playdate.scoreboards.freeScore(score)
   privatePersonalBestCallback(domainScore, $errorMessage)
 
-# proc addScore*(this: ptr PlaydateScoreboards, boardID: string, value: uint32, callback: AddScoreCallback): int32 =
-#   privateAccess(PlaydateScoreboards)
+proc invokeAddScoreCallback(score: PDScorePtr, errorMessage: ConstChar) {.cdecl, raises: [].} =
+  if errorMessage != nil:
+    privateAddScoreCallback(PDScore(value: 0, rank: 0, player: ""), $errorMessage)
+    return
+    
+  let domainScore = PDScore(value: score.value.uint32, rank: score.rank.uint32, player: $score.player)
+  playdate.scoreboards.freeScore(score)
+  privateAddScoreCallback(domainScore, $errorMessage)
 
 proc getPersonalBest*(this: ptr PlaydateScoreboards, boardID: string, callback: PersonalBestCallback): int32 =
   privateAccess(PlaydateScoreboards)
   privatePersonalBestCallback = callback
   return this.getPersonalBestBinding(boardID.cstring, invokePersonalBestCallback)
 
-# proc freeScore*(score: ptr PDScore) 
+proc addScore*(this: ptr PlaydateScoreboards, boardID: string, value: int32, callback: AddScoreCallback): int32 =
+  privateAccess(PlaydateScoreboards)
+  privateAddScoreCallback = callback
+  return this.addScoreBinding(boardID.cstring, value.uint32, invokeAddScoreCallback)
+
 # proc getScoreboards*(this: ptr PlaydateScoreboards, callback: BoardsListCallback): int32 =
 #   privateAccess(PlaydateScoreboards)
 # # proc freeBoardsList*(boardsList: ptr PDBoardsList) 
