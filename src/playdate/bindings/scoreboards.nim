29a87f13fb3d30617b9c61e5b9605e9142287e45
Nino van Hooff <ninovanhooff@gmail.com>
2024-10-25 11:11:57 +0200

    REFACTOR merge type blocks in scoreboard bindings

--- src/playdate/bindings/scoreboards.nim
+++ src/playdate/bindings/scoreboards.nim
@@ -2,36 +2,39 @@
 
 import utils
 
-type PDScoreRaw* {.importc: "PDScore", header: "pd_api.h", bycopy.} = object
-    rank* {.importc: "rank".}: cuint
-    value* {.importc: "value".}: cuint
-    player* {.importc: "player".}: cstring
+type
+    PDScoreRaw* {.importc: "PDScore", header: "pd_api.h", bycopy.} = object
+        rank* {.importc: "rank".}: cuint
+        value* {.importc: "value".}: cuint
+        player* {.importc: "player".}: cstring
 
-type PDScorePtr* = ptr PDScoreRaw
+    PDScorePtr* = ptr PDScoreRaw
 
-type PDScoresListRaw* {.importc: "PDScoresList", header: "pd_api.h", bycopy.} = object
-    boardID* {.importc: "boardID".}: cstring
-    count* {.importc: "count".}: cuint
-    lastUpdated* {.importc: "lastUpdated".}: cuint
-    playerIncluded* {.importc: "playerIncluded".}: cuint
-    limit* {.importc: "limit".}: cuint
-    scores* {.importc: "scores".}: ptr UncheckedArray[PDScoreRaw]
-type PDScoresListPtr* = ptr PDScoresListRaw
+    PDScoresListRaw* {.importc: "PDScoresList", header: "pd_api.h", bycopy.} = object
+        boardID* {.importc: "boardID".}: cstring
+        count* {.importc: "count".}: cuint
+        lastUpdated* {.importc: "lastUpdated".}: cuint
+        playerIncluded* {.importc: "playerIncluded".}: cuint
+        limit* {.importc: "limit".}: cuint
+        scores* {.importc: "scores".}: ptr UncheckedArray[PDScoreRaw]
 
-type PDBoardRaw* {.importc: "PDBoard", header: "pd_api.h", bycopy.} = object
-    boardID* {.importc: "boardID".}: cstring
-    name* {.importc: "name".}: cstring
+    PDScoresListPtr* = ptr PDScoresListRaw
 
-type PDBoardsListRaw* {.importc: "PDBoardsList", header: "pd_api.h", bycopy.} = object
-    count* {.importc: "count".}: cuint
-    lastUpdated* {.importc: "lastUpdated".}: cuint
-    boards* {.importc: "boards".}: ptr UncheckedArray[PDBoardRaw]
-type PDBoardsListPtr* = ptr PDBoardsListRaw
+    PDBoardRaw* {.importc: "PDBoard", header: "pd_api.h", bycopy.} = object
+        boardID* {.importc: "boardID".}: cstring
+        name* {.importc: "name".}: cstring
 
-type PersonalBestCallbackRaw* {.importc: "PersonalBestCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
-type AddScoreCallbackRaw* {.importc: "AddScoreCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
-type BoardsListCallbackRaw* = proc (boards: ptr PDBoardsListRaw; errorMessage: cstring) {.cdecl.}
-type ScoresCallbackRaw* = proc (scores: ptr PDScoresListRaw; errorMessage: cstring) {.cdecl.}
+    PDBoardsListRaw* {.importc: "PDBoardsList", header: "pd_api.h", bycopy.} = object
+        count* {.importc: "count".}: cuint
+        lastUpdated* {.importc: "lastUpdated".}: cuint
+        boards* {.importc: "boards".}: ptr UncheckedArray[PDBoardRaw]
+
+    PDBoardsListPtr* = ptr PDBoardsListRaw
+
+    PersonalBestCallbackRaw* {.importc: "PersonalBestCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
+    AddScoreCallbackRaw* {.importc: "AddScoreCallback", header: "pd_api.h".} = proc (score: PDScorePtr; errorMessage: cstring) {.cdecl.}
+    BoardsListCallbackRaw* = proc (boards: ptr PDBoardsListRaw; errorMessage: cstring) {.cdecl.}
+    ScoresCallbackRaw* = proc (scores: ptr PDScoresListRaw; errorMessage: cstring) {.cdecl.}
 
 sdktype:
     type PlaydateScoreboards* {.importc: "const struct playdate_scoreboards", header: "pd_api.h".} = object
